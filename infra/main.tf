41109e138aecc6ca7ded0526ed91535d1b4986e1
José Henrique <henriquesantoscjr2@hotmail.com>
2024-09-25 11:12:04 -0300

    Ajustes

--- infra/main.tf
+++ infra/main.tf
@@ -54,8 +54,7 @@ resource "aws_iam_role" "ecs_task_execution_role" {
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [{
-      Action = 
-        "sts:AssumeRole"
+      Action = "sts:AssumeRole"
       Effect = "Allow"
       Principal = {
         Service = "ecs-tasks.amazonaws.com"
