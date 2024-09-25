fc78ce32dbc824d0a2a58fc74cfb6531d71ad55d
José Henrique <henriquesantoscjr2@hotmail.com>
2024-09-25 13:31:49 -0300

    ajustes e inclusão do cloudwatch

--- src/Infra/Data/Extensions/DatabaseInitializer.cs
+++ src/Infra/Data/Extensions/DatabaseInitializer.cs
@@ -2,21 +2,26 @@
 using FIAP.TechChallenge.ByteMeBurguer.Domain.Entities;
 using FIAP.TechChallenge.ByteMeBurguer.Infra.Data.Configurations;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 
 namespace FIAP.TechChallenge.ByteMeBurguer.Infra.Data.Extensions
 {
     public class DatabaseInitializer : IDatabaseInitializer
     {
         private readonly ApplicationDbContext dbContext;
+        private readonly ILogger<DatabaseInitializer> _logger;
 
-        public DatabaseInitializer(ApplicationDbContext context)
+        public DatabaseInitializer(ApplicationDbContext context, ILogger<DatabaseInitializer> logger)
         {
             dbContext = context;
+            _logger = logger;
         }
         public void Initialize()
         {
             try
             {
+                _logger.LogInformation("Tentando conexão com o banco de dados.");
+
                 dbContext.Database.Migrate();
 
                 if (!dbContext.Categorias.Any())
@@ -31,10 +36,12 @@ namespace FIAP.TechChallenge.ByteMeBurguer.Infra.Data.Extensions
 
                     dbContext.SaveChanges();
                 }
+
+                _logger.LogInformation("Conexão com o banco de dados OK.");
             }
             catch (Exception ex)
             {
-                Console.Write(ex);
+                _logger.LogError(ex.Message);
             }
         }
     }
